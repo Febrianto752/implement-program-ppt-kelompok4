91f20a89c353e1dad1a76f9f7869fe00cc7784ac
Febrianto <febrianto.bekasi@gmail.com>
2023-06-21 09:43:36 +0700

    feat: add kiss project

---
+++ KISS/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace KISS;
+
+public class Program
+{
+    public static void WithoutKISS()
+    {
+        int item = 2;
+        string itemType = "sabun";
+        string itemName = "nuvo";
+        double diskon = 0;
+
+        if (item > 1)
+        {
+            if (itemType == "sabun")
+            {
+                if (itemName == "nuvo")
+                {
+                    diskon = 50;
+                }
+            }
+            else if (itemType == "mie")
+            {
+                if (itemName == "indomie")
+                {
+                    diskon = 30;
+                }
+            }
+        }
+
+        Console.WriteLine("Anda mendapat diskon sebesar {0}", diskon);
+    }
+
+    public static void WithKISS()
+    {
+        int item = 2;
+        string itemType = "sabun";
+        string itemName = "nuvo";
+        double diskon = 0;
+
+        if (item > 1)
+        {
+            if (itemType == "sabun" && itemName == "nuvo")
+            {
+                diskon = 50;
+            }
+            else if (itemType == "mie" && itemName == "indomie")
+            {
+                diskon = 30;
+            }
+        }
+
+        Console.WriteLine("Anda mendapat diskon sebesar {0}", diskon);
+    }
+
+
+    public static void Main(string[] args)
+    {
+
+        WithKISS();
+    }
+}
